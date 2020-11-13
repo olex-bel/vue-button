a173d55cb54054f729cb14cbf9e7856e1f8cadf6
olex-bel <a.belyaev@astoundcommerce.com>
2020-11-13 22:55:28 +0100

    Fixed navigation menu

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>|
-      <router-link to="/buttons">Buttons Demo</router-link>
+      <router-link to="/buttons">Buttons Demo</router-link>|
       <router-link to="/windbnb">Windbnb Demo</router-link>
     </div>
     <router-view />
